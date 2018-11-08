08a724374adc0e3fd3715a31d3c3f2eb6b9b95e6
Abby Sova <asova@css.edu>
2018-11-08 11:15:38 -0600

    Part 1: Trade Adjustment Decorator

---
+++ CurrencyTrader/AdjustTradeDataProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+using CurrencyTrader.Contracts;
+
+namespace CurrencyTrader
+{
+    public class AdjustTradeProvider : ITradeDataProvider
+    {
+        private readonly String url;
+
+        ITradeDataProvider urlProvider;
+
+        public AdjustTradeProvider(String url)
+        {
+            this.url = url;
+            urlProvider = new UrlTradeDatatProvider();
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            IEnumerable<string> tradeText = urlProvider.getTradeData();
+                List<string> newTradeText = new List<string>();
+
+                   foreach(string line in tradeText)
+                    {
+                        newTradeText.Add(line.Replace("GBP", "EUR"));
+                    }
+                   return newtradeText;
+                    }
+
+
+    }
+}
+
+
